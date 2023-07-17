495f066535ff5daba71b600d945ac4732dcd6212
MaybeRT-rt <maybe.rt@icloud.com>
2023-07-17 19:35:16 +0300

    add tabbarcontroller

--- MusicApp/MainTabBarController.swift
+++ MusicApp/MainTabBarController.swift
@@ -11,8 +11,25 @@ class MainTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .lightGray
+        UITabBar.appearance().backgroundColor = .white
+        tabBar.tintColor = .systemIndigo
+        creatTabBarControllers()
+    }
+    
+    private func creatTabBarControllers() {
+        viewControllers = [generateViewController(rootViewController: SearchViewController(), image: UIImage(systemName: "magnifyingglass") ?? UIImage(), title: "Search"), generateViewController(rootViewController: LibraryViewController(), image: UIImage(systemName: "music.note.list") ?? UIImage(), title: "Library")]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        
+        return navigationVC
     }
     
 }
---
+++ MusicApp/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  MusicApp
+//
+//  Created by Liz-Mary on 17.07.2023.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray5
+    
+    }
+    
+}

c4ef8f687692669f4df14193091b4cea67d3d9fe
MaybeRT-rt <maybe.rt@icloud.com>
2023-07-18 11:48:52 +0300

    add UISearchController

--- MusicApp/SearchViewController.swift
+++ MusicApp/SearchViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class SearchViewController: UITableViewController {
     
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tracks = [TrackModel(trackName: "Love", artistName: "Jack"),
+                      TrackModel(trackName: "Black", artistName: "B&W")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemGray5
-    
+        
+        setupSearchBar()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "indexId")
     }
     
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        
+        searchController.searchBar.delegate = self
+    }
+    
+}
+
+extension SearchViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "indexId", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = UIImage(named: "bug")
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
 }
---
+++ MusicApp/TrackModel.swift
@@ -0,0 +1,13 @@
+//
+//  TrackModel.swift
+//  MusicApp
+//
+//  Created by Liz-Mary on 18.07.2023.
+//
+
+import Foundation
+
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}

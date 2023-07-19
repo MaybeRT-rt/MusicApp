5409619b55d0d296a7cd2b6f8a59628f1e624b71
MaybeRT-rt <maybe.rt@icloud.com>
2023-07-19 14:23:52 +0300

    json(iTunes Search API) decoded in model date

--- MusicApp/SearchViewController.swift
+++ MusicApp/SearchViewController.swift
@@ -10,10 +10,11 @@ import Alamofire
 
 class SearchViewController: UITableViewController {
     
+    private var timer: Timer?
+    
     let searchController = UISearchController(searchResultsController: nil)
     
-    let tracks = [TrackModel(trackName: "Love", artistName: "Jack"),
-                      TrackModel(trackName: "Black", artistName: "B&W")]
+    var tracks = [Track]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +42,7 @@ extension SearchViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "indexId", for: indexPath)
         let track = tracks[indexPath.row]
-        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.text = "\(String(describing: track.trackName))\n\(String(describing: track.artistName))"
         cell.textLabel?.numberOfLines = 2
         cell.imageView?.image = UIImage(named: "bug")
         return cell
@@ -51,18 +52,35 @@ extension SearchViewController {
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-    
-        let url = "https://itunes.apple.com/search?term=\(searchText)"
         
-        AF.request(url).responseData { (responseDatas) in
-            if let error = responseDatas.error {
-                print("error reveiced responsing data \(error.localizedDescription)")
-                return
-            }
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
             
-            guard let data = responseDatas.data else { return }
-            let someString = String(data: data, encoding: .utf8)
-            print(someString ?? "")
-        }
+            let url = "https://itunes.apple.com/search"
+            
+            let parameters = ["term":"\(searchText)", "limit":"10"]
+            
+            AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (responseDatas) in
+                if let error = responseDatas.error {
+                    print("error reveiced responsing data \(error.localizedDescription)")
+                    return
+                }
+                guard let data = responseDatas.data else { return }
+                
+                let decoder = JSONDecoder()
+                
+                do {
+                    let objects = try decoder.decode(SearchResponse.self, from: data)
+                    print("objects, \(objects)")
+                    self.tracks = objects.results
+                    self.tableView.reloadData()
+                    
+                } catch let jsonError {
+                    print("Failed to decode JSON", jsonError)
+                }
+                
+            }
+        })
     }
 }
+
